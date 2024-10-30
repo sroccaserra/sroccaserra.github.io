99041a7bdfd3cd3038e520ce201f97b211aa6769
sroccaserra <sroccaserra@yahoo.com>
2024-10-30 16:41:04 +0100

    r Better publishing script output

--- scripts/publish_srht.sh
+++ scripts/publish_srht.sh
@@ -1,7 +1,14 @@
-set -x
-tar -C ${BUILD} -cvz . > site.tar.gz
+#!/bin/env bash
+
+set -eu -o pipefail
+
+echo "Archiving ./${BUILD}..."
+tar -C ${BUILD} -cz . > site.tar.gz
+
+echo "Publishing ${PROTOCOL:-HTML}..."
+
 source .env && \
     curl --oauth2-bearer "$srht_token" \
     -Fcontent=@site.tar.gz \
-    -Fprotocol=${PROTOCOL} \
+    -Fprotocol=${PROTOCOL:-} \
     https://pages.sr.ht/publish/sroccaserra.srht.site
